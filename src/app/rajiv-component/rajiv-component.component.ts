dd941aa911bc7e9d43b818a166602dc5deda5ba6
Rajiv Mirpuri <rajiv.mirpuri@gmail.com>
2018-09-22 16:38:18 -0400

    Lecture 26 Event binding

--- src/app/rajiv-component/rajiv-component.component.ts
+++ src/app/rajiv-component/rajiv-component.component.ts
@@ -11,13 +11,19 @@ export class RajivComponentComponent implements OnInit {
 
   allowNewServer = false;
 
+  serverCreationStatus = "No server was created";
+
   constructor() {
 
     setTimeout(  () => {
       this.allowNewServer = true;
     } , 2000);
-
   }
+
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created";
+  }
+
 
   ngOnInit() {
   }
